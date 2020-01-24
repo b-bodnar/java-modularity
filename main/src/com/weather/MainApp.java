f439dd2b31a194fb5ec8d9b79a2c84d0fc5f86e9
Bohdan Bodnar <iloveyoulviv@gmail.com>
2020-01-24 09:52:52 +0200

    Add module main

---
+++ main/src/com/weather/MainApp.java
@@ -0,0 +1,9 @@
+package com.weather;
+
+import com.weather.execute.Executor;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Executor.execute();
+    }
+}
---
+++ main/src/com/weather/execute/Executor.java
@@ -0,0 +1,8 @@
+package com.weather.execute;
+
+public class Executor {
+
+    public static void execute() {
+        System.out.println("Sunny weather!");
+    }
+}
